26b4aa36ebcb290f4d199aeba198f2d99d69b196
jo <jfranchetti@spotify.com>
2023-04-14 17:39:00 +0100

    fixing types

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,32 @@
-export * from "./SpotifyApi";
-export * from "./auth/AuthorizationCodeWithPKCEStrategy";
-export * from "./auth/ClientCredentialsStrategy";
-export * from "./auth/IAuthStrategy";
-export * from "./caching/InMemoryCachingStrategy";
-export * from "./caching/LocalStorageCachingStrategy";
-export * from "./caching/ICacheStore";
-export * from "./caching/GenericCache";
-export * from "./errorhandling/ConsoleLoggingErrorHandler";
-export * from "./errorhandling/NoOpErrorHandler";
-export * from "./redirection/DocumentLocationRedirectionStrategy";
-export * from "./responsevalidation/DefaultResponseValidator";
-export * from "./serialization/DefaultResponseDeserializer";
+import { SpotifyApi } from "./SpotifyApi";
+import AuthorizationCodeWithPKCEStrategy from "./auth/AuthorizationCodeWithPKCEStrategy";
+import ClientCredentialsStrategy from "./auth/ClientCredentialsStrategy";
+import IAuthStrategy from "./auth/IAuthStrategy";
+import InMemoryCachingStrategy from "./caching/InMemoryCachingStrategy";
+import LocalStorageCachingStrategy from "./caching/LocalStorageCachingStrategy";
+import { ICacheStore } from "./caching/ICacheStore";
+import GenericCache from "./caching/GenericCache";
+import ConsoleLoggingErrorHandler from "./errorhandling/ConsoleLoggingErrorHandler";
+import NoOpErrorHandler from "./errorhandling/NoOpErrorHandler";
+import DocumentLocationRedirectionStrategy from "./redirection/DocumentLocationRedirectionStrategy";
+import DefaultResponseValidator from "./responsevalidation/DefaultResponseValidator";
+import DefaultResponseDeserializer from "./serialization/DefaultResponseDeserializer";
 
+export {
+    SpotifyApi,
+    AuthorizationCodeWithPKCEStrategy,
+    ClientCredentialsStrategy,
+    InMemoryCachingStrategy,
+    LocalStorageCachingStrategy,
+    GenericCache,
+    ConsoleLoggingErrorHandler,
+    NoOpErrorHandler,
+    DocumentLocationRedirectionStrategy,
+    DefaultResponseValidator,
+    DefaultResponseDeserializer
+}
+
+export type {
+    IAuthStrategy,
+    ICacheStore
+}
