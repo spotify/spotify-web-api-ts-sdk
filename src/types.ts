e086663221392fc15c267c9bc634e577c5333d9e
jo <jfranchetti@spotify.com>
2023-06-30 18:42:32 +0100

    updating player endpoints with accurate typings. Credit to @sspenst

--- src/types.ts
+++ src/types.ts
@@ -619,3 +619,73 @@ export interface Tatum {
     duration: number
     confidence: number
 }
+
+export interface PlaybackState {
+    device: Device
+    repeat_state: string
+    shuffle_state: boolean
+    context: Context | null
+    timestamp: number
+    progress_ms: number
+    is_playing: boolean
+    item: Track | Episode
+    currently_playing_type: string
+    actions: Actions
+}
+
+export interface Device {
+    id: string | null
+    is_active: boolean
+    is_private_session: boolean
+    is_restricted: boolean
+    name: string
+    type: string
+    volume_percent: number | null
+}
+
+export interface Devices {
+    devices: Device[]
+}
+
+export interface Context {
+    type: string
+    href: string
+    external_urls: ExternalUrls
+    uri: string
+}
+
+export interface Actions {
+    interrupting_playback?: boolean
+    pausing?: boolean
+    resuming?: boolean
+    seeking?: boolean
+    skipping_next?: boolean
+    skipping_prev?: boolean
+    toggling_repeat_context?: boolean
+    toggling_shuffle?: boolean
+    toggling_repeat_track?: boolean
+    transferring_playback?: boolean
+}
+
+export interface RecentlyPlayedTracksPage {
+    href: string
+    limit: number
+    next: string | null
+    cursors: {
+        after: string
+        before: string
+    }
+    total: number
+    items: PlayHistory[]
+}
+
+export interface PlayHistory {
+    track: Track
+    played_at: string
+    context: Context
+}
+
+export interface Queue {
+    currently_playing: Track | Episode | null
+    queue: (Track | Episode)[]
+}
