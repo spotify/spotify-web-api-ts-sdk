7c2f2829d473ca9707e16d20391667b6ebe49c16
abcdmku <63693423+abcdmku@users.noreply.github.com>
2023-09-21 23:24:05 -0500

    Fix TS error on SearchEndpoints
    const was causing a type error

--- src/endpoints/SearchEndpoints.ts
+++ src/endpoints/SearchEndpoints.ts
@@ -2,11 +2,11 @@ import type { ItemTypes, Market, MaxInt, SearchResults } from '../types.js';
 import EndpointsBase from './EndpointsBase.js';
 
 export interface SearchExecutionFunction {
-    <const T extends readonly ItemTypes[]>(q: string, type: T, market?: Market, limit?: MaxInt<50>, offset?: number, include_external?: string): Promise<SearchResults<T>>;
+    <T extends readonly ItemTypes[]>(q: string, type: T, market?: Market, limit?: MaxInt<50>, offset?: number, include_external?: string): Promise<SearchResults<T>>;
 }
 
 export default class SearchEndpoints extends EndpointsBase {
-    public async execute<const T extends readonly ItemTypes[]>(q: string, type: T, market?: Market, limit?: MaxInt<50>, offset?: number, include_external?: string) {
+    public async execute<T extends readonly ItemTypes[]>(q: string, type: T, market?: Market, limit?: MaxInt<50>, offset?: number, include_external?: string) {
         const params = this.paramsFor({ q, type, market, limit, offset, include_external });
         return await this.getRequest<SearchResults<T>>(`search${params}`);
     }
